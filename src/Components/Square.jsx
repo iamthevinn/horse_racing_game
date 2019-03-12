576db688c1666b702a8b269ec96ca4206d4249cc
Vince Carini <vince.carini@gmail.com>
2019-03-11 20:42:05 -0500

    Heads are moving

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setHorsePosition } from '../Actions/GamePlayActions';
+import horse_head from '../Assets/horse_head.png'
 
 class Square extends Component {
   constructor(props) {
@@ -15,9 +16,18 @@ class Square extends Component {
 
   render() {
     const { subsequent, horse, position, occupied } = this.props;
-    const squareStyle = subsequent ? (occupied ? "SubsequentSquare OccupiedSquare" : "SubsequentSquare UnoccupiedSquare") : (occupied ? "OccupiedSquare" : "UnoccupiedSquare");
+    //const squareStyle = subsequent ? (occupied ? "SubsequentSquare OccupiedSquare" : "SubsequentSquare UnoccupiedSquare") : (occupied ? "OccupiedSquare" : "UnoccupiedSquare");
+    const squareStyle = subsequent ? "SubsequentSquare Square" : "Square";
     return (
-      <div className={squareStyle} onClick={() => this.setHorsePostion(horse, position)}>
+      <div>
+        {
+          !occupied ?
+            <div className={squareStyle} onClick={() => this.setHorsePostion(horse, position)} />
+            :
+            <div className={squareStyle} onClick={() => this.setHorsePostion(horse, position)}>
+              <img className={"HorseImage"} alt="horse head" src={horse_head} />
+            </div>
+        }
       </div>
     );
   }
