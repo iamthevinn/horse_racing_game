9cee338af33a1eb9ad6a27085b370b7870ef3e31
Vince Carini <vince.carini@gmail.com>
2019-03-12 21:10:55 -0500

    Remove Console log

--- src/Actions/GamePlayActions.js
+++ src/Actions/GamePlayActions.js
@@ -4,7 +4,6 @@ export const SET_POSITION = "SET_POSITION";
 export function setHorsePosition(postPosition, squarePosition) {
   return (dispatch, getState, api) => {
     const horsePosition = { postPosition, squarePosition };
-    console.log('horsePosition', horsePosition)
     dispatch({ type: SET_POSITION, data: horsePosition });
   }
 }
--- src/Reducers/GamePlayReducer.js
+++ src/Reducers/GamePlayReducer.js
@@ -12,9 +12,6 @@ export function gamePlayReducer(state = initialGameState, action) {
     case SET_POSITION:
       const higherHorses = state.horsePositions.slice(action.data.postPosition - 1);
       const lowerHorses = state.horsePositions.slice(0, action.data.postPosition - 2);
-      console.log(higherHorses);
-      console.log(lowerHorses);
-      console.log(action.data.distance)
       return { ...state, horsePositions: lowerHorses.concat([action.data.squarePosition]).concat(higherHorses) };
     default:
       return state;
