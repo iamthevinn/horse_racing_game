23fc2ff26e82ae22a3f2df2347389f72399cceff
Vince Carini <vince.carini@gmail.com>
2019-03-21 22:07:13 -0500

    Remove comment

--- src/Reducers/BoardReducer.js
+++ src/Reducers/BoardReducer.js
@@ -3,7 +3,7 @@ import { SET_POSITION_ON_BOARD } from '../Actions/BoardActions'
 export const initialBoardState = {
   horsePositions: [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
 };
-// {postPosition: 4, distance: 2}
+
 export function boardReducer(state = initialBoardState, action) {
   switch (action.type) {
     case SET_POSITION_ON_BOARD:
