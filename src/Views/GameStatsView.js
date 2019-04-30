677dc308846a6b0cfd36dc07d51875c9085fec19
Vince Carini <vince.carini@gmail.com>
2019-04-29 21:55:02 -0500

    One Chart Working

--- src/Views/GameStatsView.js
+++ src/Views/GameStatsView.js
@@ -1,24 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { Chart, ArgumentAxis, ValueAxis, LineSeries, BarSeries } from "@devexpress/dx-react-chart-material-ui";
+import Paper from '@material-ui/core/Paper';
+import {
+  Chart,
+  ArgumentAxis,
+  ValueAxis,
+  BarSeries,
+  Legend,
+} from '@devexpress/dx-react-chart-material-ui';
+import { ValueScale, Animation } from '@devexpress/dx-react-chart';
+import { getDiceRollsByFrequency, getMoneyPaidByRace, getWinnersByFrequency } from '../Stats/StatsHelper'
+
+const Point = (props) => {
+  const { style, ...restProps } = props;
+  return (
+    <BarSeries.Point
+      style={{ ...style, animationDuration: `${(restProps.index + 1) * 0.3}s` }}
+      {...restProps}
+    />
+  );
+};
 
 
 class GameStatsView extends Component {
   render() {
+    const { history } = this.props;
+    console.log()
     return (
       <div>
-        <div style={{ width: "50%"}}>
-          <Chart 
-            data={[
-              { argument: 1, value: 10 },
-              { argument: 2, value: 20 },
-              { argument: 3, value: 30 }
-            ]}
-          >
-            <ArgumentAxis />
-            <ValueAxis />
-            <LineSeries valueField="value" argumentField="argument" />
-          </Chart>
+        <div>
+          <Paper>
+            <Chart
+              data={getDiceRollsByFrequency(history)}
+            >
+              <ValueScale name="frequency" />
+              <ValueScale name="diceTotal" />
+
+              <ArgumentAxis />
+              <ValueAxis scaleName="frequency" position={"left"} showGrid={true} showLine showTicks />
+
+              <BarSeries
+                name="Times Rolled"
+                valueField="frequency"
+                argumentField="diceTotal"
+                scaleName="frequency"
+                pointComponent={Point}
+                width='30px'
+              />
+              <Animation />
+              <Legend />
+            </Chart>
+          </Paper>
         </div>
       </div>
     );
@@ -27,7 +59,7 @@ class GameStatsView extends Component {
 
 const mapStateToProps = state => {
   return {
-
+    history: state.gamePlayReducer.history
   };
 };
 
